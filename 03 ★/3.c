f8791129c4e2b00583d871bcf539a34486a8f44c
Ciremun <Ciremun@yandex.ru>
2021-01-19 23:21:21 +0300

    pepega

--- 03 ★/3.c
+++ 03 ★/3.c
@@ -11,13 +11,17 @@
 #define MAX_INTERSECT_COUNT 512
 #define GRID_SIZE_X 65536
 #define GRID_SIZE_Y 65536
-#define START_X 32768
-#define START_Y 32768
+#define START 0
+// 32768
 
 #define UP 'U'
 #define DOWN 'D'
 #define LEFT 'L'
 #define RIGHT 'R'
+
+bool found_intersection = false;
+int first_intersection[1][2];
+int steps = 0;
 
 int calc_manhattan(int p1, int p2, int q1, int q2)
 {
@@ -54,16 +58,36 @@ void read_file(const char *fp, char lines[LINES_COUNT][LINE_LENGTH])
 
 void add_intersect(int i[MAX_INTERSECT_COUNT][2], int *ic, int x, int y)
 {
+    if (!found_intersection && x != START && y != START)
+    {
+        printf("fount first intersect (%d, %d) in %d steps\n", x, y, steps);
+        found_intersection = true;
+    }
     i[*ic][0] = x;
     i[*ic][1] = y;
     (*ic)++;
+}
+
+int parse_num(char lines[LINES_COUNT][LINE_LENGTH], int *cursor, int line_pos, char eol)
+{
+    char num_s[MAX_NUM_SIZE] = {"\0"};
+    int num_cursor = 0;
+    (*cursor)++;
+    while (lines[line_pos][*cursor] != eol && lines[line_pos][*cursor] != ',')
+    {
+        num_s[num_cursor] = lines[line_pos][*cursor];
+        (*cursor)++;
+        num_cursor++;
+    }
+    num_s[num_cursor] = '\0';
+    return (int)strtol(num_s, NULL, 10);
 }
 
 void part_1(char lines[LINES_COUNT][LINE_LENGTH])
 {
     int op_count = 0;
     int path[MAX_OP_COUNT][2];
-    fill_2d_arr(path, MAX_OP_COUNT, 32768);
+    fill_2d_arr(path, MAX_OP_COUNT, START);
     bool(*occupied)[GRID_SIZE_Y] = malloc(sizeof(bool) * GRID_SIZE_X * GRID_SIZE_Y);
     for (int cursor = 0; lines[0][cursor] != '\0'; cursor++)
     {
@@ -72,17 +96,7 @@ void part_1(char lines[LINES_COUNT][LINE_LENGTH])
             memcpy(path[op_count], path[op_count - 1], sizeof(int) * 2);
         }
         char op = lines[0][cursor];
-        char num_s[MAX_NUM_SIZE] = {"\0"};
-        int num_cursor = 0;
-        cursor++;
-        while (lines[0][cursor] != '\n' && lines[0][cursor] != ',')
-        {
-            num_s[num_cursor] = lines[0][cursor];
-            cursor++;
-            num_cursor++;
-        }
-        num_s[num_cursor] = '\0';
-        int num = (int)strtol(num_s, NULL, 10);
+        int num = parse_num(lines, &cursor, 0, '\n');
         switch (op)
         {
         case UP:
@@ -93,14 +107,14 @@ void part_1(char lines[LINES_COUNT][LINE_LENGTH])
             path[op_count][1] += num;
             break;
         case DOWN:
-            for (int i = path[op_count][1] - num; i <= path[op_count][1]; i++)
+            for (int i = path[op_count][1]; i >= path[op_count][1] - num; i--)
             {
                 occupied[path[op_count][0]][i] = true;
             }
             path[op_count][1] -= num;
             break;
         case LEFT:
-            for (int i = path[op_count][0] - num; i <= path[op_count][0]; i++)
+            for (int i = path[op_count][0]; i >= path[op_count][0] - num; i--)
             {
                 occupied[i][path[op_count][1]] = true;
             }
@@ -124,8 +138,10 @@ void part_1(char lines[LINES_COUNT][LINE_LENGTH])
     int path_2[MAX_OP_COUNT][2];
     int intersect[MAX_INTERSECT_COUNT][2];
     int intersect_count = 0;
-    fill_2d_arr(path_2, MAX_OP_COUNT, 32768);
-    fill_2d_arr(intersect, MAX_INTERSECT_COUNT, 32768);
+    fill_2d_arr(path_2, MAX_OP_COUNT, START);
+    fill_2d_arr(intersect, MAX_INTERSECT_COUNT, START);
+
+    char prev_op = '\0';
 
     for (int cursor = 0; lines[1][cursor] != '\0'; cursor++)
     {
@@ -134,56 +150,75 @@ void part_1(char lines[LINES_COUNT][LINE_LENGTH])
             memcpy(path_2[op_count], path_2[op_count - 1], sizeof(int) * 2);
         }
         char op = lines[1][cursor];
-        char num_s[MAX_NUM_SIZE] = {"\0"};
-        int num_cursor = 0;
-        cursor++;
-        while (lines[1][cursor] != '\0' && lines[1][cursor] != ',')
-        {
-            num_s[num_cursor] = lines[1][cursor];
-            cursor++;
-            num_cursor++;
-        }
-        num_s[num_cursor] = '\0';
-        int num = (int)strtol(num_s, NULL, 10);
+        int num = parse_num(lines, &cursor, 1, '\0');
+        bool corner = false;
         switch (op)
         {
         case UP:
+            corner = (prev_op == LEFT || prev_op == RIGHT);
+            if (corner)
+            {
+                steps--;
+            }
             for (int i = path_2[op_count][1]; i <= path_2[op_count][1] + num; i++)
             {
                 if (occupied[path_2[op_count][0]][i])
                 {
                     add_intersect(intersect, &intersect_count, path_2[op_count][0], i);
                 }
+                steps++;
+                // printf("step %d (%d, %d) %c\n", steps, path_2[op_count][0], i, op);
             }
             path_2[op_count][1] += num;
             break;
         case DOWN:
-            for (int i = path_2[op_count][1] - num; i <= path_2[op_count][1]; i++)
+            corner = (prev_op == LEFT || prev_op == RIGHT);
+            if (corner)
+            {
+                steps--;
+            }
+            for (int i = path_2[op_count][1]; i >= path_2[op_count][1] - num; i--)
             {
                 if (occupied[path_2[op_count][0]][i])
                 {
                     add_intersect(intersect, &intersect_count, path_2[op_count][0], i);
                 }
+                steps++;
+                // printf("step %d (%d, %d) %c\n", steps, path_2[op_count][0], i, op);
             }
             path_2[op_count][1] -= num;
             break;
         case LEFT:
-            for (int i = path_2[op_count][0] - num; i <= path_2[op_count][0]; i++)
+            corner = (prev_op == UP || prev_op == DOWN);
+            if (corner)
+            {
+                steps--;
+            }
+            for (int i = path_2[op_count][0]; i >= path_2[op_count][0] - num; i--)
             {
                 if (occupied[i][path_2[op_count][1]])
                 {
                     add_intersect(intersect, &intersect_count, i, path_2[op_count][1]);
                 }
+                steps++;
+                // printf("step %d (%d, %d) %c\n", steps, i, path_2[op_count][1], op);
             }
             path_2[op_count][0] -= num;
             break;
         case RIGHT:
+            corner = (prev_op == UP || prev_op == DOWN);
+            if (corner)
+            {
+                steps--;
+            }
             for (int i = path_2[op_count][0]; i <= path_2[op_count][0] + num; i++)
             {
                 if (occupied[i][path_2[op_count][1]])
                 {
                     add_intersect(intersect, &intersect_count, i, path_2[op_count][1]);
                 }
+                steps++;
+                // printf("step %d (%d, %d) %c\n", steps, i, path_2[op_count][1], op);
             }
             path_2[op_count][0] += num;
             break;
@@ -191,22 +226,35 @@ void part_1(char lines[LINES_COUNT][LINE_LENGTH])
             printf("error: unknown operation: %c\n", op);
             exit(1);
         }
+        prev_op = op;
         op_count++;
     }
     int result = INT_MAX;
     for (int i = 0; i < MAX_INTERSECT_COUNT; i++)
     {
-        if (intersect[i][0] == START_X && intersect[i][1] == START_Y)
+        if (intersect[i][0] == START && intersect[i][1] == START)
         {
             continue;
         }
-        int distance = calc_manhattan(START_X, START_Y, intersect[i][0], intersect[i][1]);
+        // printf("(%d, %d)\n", intersect[i][0], intersect[i][1]);
+        int distance = calc_manhattan(START, START, intersect[i][0], intersect[i][1]);
         if (result > distance)
         {
             result = distance;
         }
     }
     printf("part 1: %d\n", result);
+
+
+
+    // bool(*occupied_2)[GRID_SIZE_Y] = malloc(sizeof(bool) * GRID_SIZE_X * GRID_SIZE_Y);
+    // op_count = 0;
+    // fill_2d_arr(path, MAX_OP_COUNT, START);
+    // intersect_count = 0;
+    // fill_2d_arr(path_2, MAX_OP_COUNT, START);
+    // fill_2d_arr(intersect, MAX_INTERSECT_COUNT, START);
+
+
 }
 
 int main()
